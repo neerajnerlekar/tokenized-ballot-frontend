79c652eadbf4c70c7d76ded489801675e5785a12
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-05-22 20:20:33 -0600

    update the BalanceComponent API endpoint to balanceOf

--- components/BalanceComponent.jsx
+++ components/BalanceComponent.jsx
@@ -16,7 +16,7 @@ const BalanceComponent = () => {
       }
       setIsLoading(true) // Set loading state to true
       const response = await axios.get(
-        `http://localhost:3001/balance/${signer._address}`
+        `http://localhost:3001/balanceOf/${signer._address}`
       ) // <-- Use signer's address
       const balanceValue = ethers.BigNumber.from(response.data.hex)
       const formattedBalance = ethers.utils.formatEther(balanceValue)
